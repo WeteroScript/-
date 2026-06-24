6db5cd3522a3e5793bda0bd8e12b5c540b82f988
WeteroScript <weterochina@gmail.com>
2026-06-24 17:39:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MAX_THREADS = int(os.getenv("MAX_THREADS", 500))
-TIMEOUT = int(os.getenv("TIMEOUT", 3))
+MAX_THREADS = int(os.getenv("MAX_THREADS", 2000))   # увеличен до 2000
+TIMEOUT = int(os.getenv("TIMEOUT", 1))              # таймаут 1 сек
+SYN_COUNT_RATIO = 0.5    # 50% на SYN
+ICMP_COUNT_RATIO = 0.25  # 25% на ICMP
